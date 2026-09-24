51be923786533faa86779f7c7410bff55f5670a6
Zse <zse@zse.pl>
2026-09-24 12:28:23 +0200

    koniec lekcji

---
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,38 @@
+// VariableConsoleApplication.cpp 
+
+#include <iostream>
+
+//Napisz program który wczyta liczbę od użytkownika 
+// i ją wyświetli na konsoli
+int main()
+{
+	//wczytanie liczby od użytkownika
+	//	informacja co chcemy
+	std::cout << "Podaj liczbę:\n";
+	//  pobieramy daną
+	//     deklaracja zmiennej
+	
+	//     zapamiętanie danej
+	std::cin >> 
+	//wyświetlenie na konsoli
+
+}
+
+/*
+Algorytm - skończony zbiór instrukcji,
+który rozwiązuje zadany problem.
+Określa też kolejność wynonywanych instrukcji.
+Zapis algorytmu:
+* opis słowny
+* w punktach
+* rysunki
+* schemat blokowy
+* kod źródłowy danego języka programowania
+* pseudokod
+
+Zmienna - pewien obszar w pamięci operacyjnej, w której można
+w danej chwili przechować tylko jedną daną.
+
+Instrukcja daklaracji zmiennej:
+
+*/
